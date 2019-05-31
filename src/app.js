819ae815fa0edbb45be213ab9ddcf06883091c00
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2019-05-31 15:22:31 -0300

    organizing redux

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ import React from 'react'
 import ReactDOM from "react-dom"
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-import AppRouter from "./routers/AppRouter";
+import AppRouter from "./routers/AppRouter"
+import configureStore from './store/configureStore'
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from './selectors/expenses'
+
+const store = configureStore()
+
+store.dispatch(addExpense({ description: 'Water bill' }))
+store.dispatch(addExpense({ description: 'Gas bill' }))
+store.dispatch(setTextFilter('water'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
+// console.log(store.getState())
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
