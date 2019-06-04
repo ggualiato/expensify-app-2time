aea12d87a638f8ba8e6bd04494856a60a98e8708
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2019-06-04 17:17:36 -0300

    rendering individual expenses

--- src/app.js
+++ src/app.js
@@ -11,12 +11,12 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water bill' }))
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
 store.dispatch(addExpense({ description: 'Gas bill' }))
 store.dispatch(setTextFilter('water'))
 
 setTimeout(() => {
-    store.dispatch(setTextFilter('rent'))
+    store.dispatch(setTextFilter('bill'))
 }, 3000)
 
 const state = store.getState()
