257d5e024ca6abf66f1e23826c7398ecab9b4970
Giovanni Gualiato <ggualiato@gmail.com>
2019-07-31 22:42:05 -0300

    connecting ExpenseListItem with store

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ description, amount, createdAt, dispatch, id }) => (
     <div>
         <h3>{description}</h3>
         <p>{amount} - {createdAt}</p>
+        <button onClick={() => dispatch(removeExpense({ id }))}>Remove</button>
     </div>
 )
 
-export default ExpenseListItem
+export default connect()(ExpenseListItem)
