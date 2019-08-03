1cd7154c7e744b567845926992277ffbd0895d7b
Giovanni Gualiato <ggualiato@gmail.com>
2019-08-03 20:42:29 -0300

    fix error

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -12,7 +12,7 @@ export default class ExpenseForm extends Component {
             description: props.expense ? props.expense.description : '',
             note: props.expense ? props.expense.note : '',
             amount: props.expense ? (props.expense.amount / 100).toString() : '',
-            createdAt: props.expense.createdAt ? moment(props.expense.createdAt) : moment(),
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calendarFocused: false,
             error: ''
         }
