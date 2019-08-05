b086356782872d30872f56b9902d13d7e1f3f81c
Giovanni Gualiato <ggualiato@gmail.com>
2019-08-04 22:30:59 -0300

    add express

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+
+const publicPath = path.join(__dirname, '..', 'public')
+
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(3000, () => {
+    console.log('Server is up');
+})
