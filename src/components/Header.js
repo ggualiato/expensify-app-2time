5153d4f8da4f4edb055450ec1c41deffde492a31
Giovanni Gualiato <ggualiato@gmail.com>
2020-01-13 20:41:54 -0300

    setup for enzyme and snapshot test

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,10 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        
     </header>
 )
+
+{/* <NavLink to="/help" activeClassName="is-active">Help</NavLink> */}
 
 export default Header
