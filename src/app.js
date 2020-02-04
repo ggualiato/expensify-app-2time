ec92ee925935e71f0d4a0504474558127de03f6e
Giovanni Gualiato <ggualiato@gmail.com>
2020-02-03 23:15:46 -0300

    mocking moment for snapshot tests

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
+import 'react-dates/initialize'
 
 const store = configureStore()
 
