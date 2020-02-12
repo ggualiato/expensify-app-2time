9233ca1c2e703138b210183cd78e5e65410251a8
Giovanni Gualiato <ggualiato@gmail.com>
2020-02-12 20:36:33 -0300

    FC to Class Component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,21 +1,25 @@
-import React from "react"
+import React, { Component } from "react"
 import ExpenseForm from "./ExpenseForm";
 import { connect } from "react-redux";
 import { addExpense } from "../actions/expenses";
 
+export class AddExpensePage extends Component {
+    onSubmit = expense => {
+        this.props.addExpense(expense)
+        this.props.history.push('/')
+    }
 
-
-export const AddExpensePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm
-            onSubmit={expense => {
-                props.addExpense(expense)
-                props.history.push('/')
-            }}
-        />
-    </div>
-)
+    render() {
+        return (
+            <div>
+                <h1>Add Expense</h1>
+                <ExpenseForm
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        )
+    }
+}
 
 const mapDispatchToProps = (dispatch) => ({
     addExpense: (expense) => dispatch(addExpense(expense))
