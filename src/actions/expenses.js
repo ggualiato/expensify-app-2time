b7aecb89e093d2991af3ede70611993cb8fd7cb9
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2020-07-18 16:49:58 -0300

    Adding asynchronous redux actions

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,37 +1,37 @@
-import uuid from 'uuid'
-import database from '../firebase/firebase'
+import uuid from "uuid";
+import database from "../firebase/firebase";
 
 export const addExpense = (expense) => ({
-    type: 'ADD_EXPENSE',
-    expense
-})
+    type: "ADD_EXPENSE",
+    expense,
+});
 
 export const startAddExpense = (expenseData = {}) => {
     return (dispatch) => {
-        const {
-            description = '',
-            note = '',
-            amount = 0,
-            createdAt = 0
-        } = expenseData
-        const expense = { description, note, amount, createdAt }
-        database.ref('expenses').push(expense).then(ref => {
-            dispatch(addExpense({
-                id: ref.key,
-                ...expense
-            }))
-        })
-    }
-}
+        const { description = "", note = "", amount = 0, createdAt = 0 } = expenseData;
+        const expense = { description, note, amount, createdAt };
 
+        return database
+            .ref("expenses")
+            .push(expense)
+            .then((ref) => {
+                dispatch(
+                    addExpense({
+                        id: ref.key,
+                        ...expense,
+                    })
+                );
+            });
+    };
+};
 
 export const removeExpense = ({ id } = {}) => ({
-    type: 'REMOVE_EXPENSE',
-    id
-})
+    type: "REMOVE_EXPENSE",
+    id,
+});
 
 export const editExpense = (id, updates) => ({
-    type: 'EDIT_EXPENSE',
+    type: "EDIT_EXPENSE",
     id,
-    updates
-})
+    updates,
+});
